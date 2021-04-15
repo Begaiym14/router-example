42409d3182a2d67eb290aa1ee9c9bdab507ae924
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-04-15 13:57:34 +0600

    changed App.js and App.css

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Link to="/" >Home</Link>
-      <Link to="/about" >About</Link>
-      <Link to="/contacts" >Contacts</Link>
+      <Link className="Link" to="/" >Home</Link>
+      <Link className="Link" to="/about" >About</Link>
+      <Link className="Link" to="/contacts" >Contacts</Link>
 
-      <Route path="/" component={Home} exact />
-      <Route path="/about" component={About} />
-      <Route path="/contacts" component={Contacts} />
+      <Route className="Route" path="/" component={Home} exact />
+      <Route className="Route" path="/about" component={About} />
+      <Route className="Route" path="/contacts" component={Contacts} />
 
     </div>
   );
