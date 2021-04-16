20194633817c7e0133c868c2d9ddc0170d1caba7
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-04-16 14:01:32 +0600

    added Users.js and Users.module.css

---
+++ src/components/Users/Users.js
@@ -0,0 +1,14 @@
+import classes from "./Users.module.css";
+
+
+const Users = ({  }) => {
+  
+  return (
+    <div className={classes.Users}>
+     <h1>Users</h1> 
+     <p>Shaibekova Begaiym</p>
+    </div>
+  );
+}
+
+export default Users;
